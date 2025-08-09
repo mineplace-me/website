f3227b1a0840523d5a054d900f95c7a627283e41
saboooor <saboor547@gmail.com>
2025-08-09 06:48:48 -0600

    a bit better looking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,43 @@
-import { component$, useSignal } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Blocks, Box, Cuboid, Hand } from 'lucide-icons-qwik';
+import { Box, Github, Network } from 'lucide-icons-qwik';
+import { LogoBirdflop, LogoDiscord, LogoLuminescent } from '@luminescent/ui-qwik';
 
 export default component$(() => {
   return (
     <div class="flex flex-col items-center justify-center min-h-screen">
-      <div class="lum-card p-24 shadow-xl">
-        <h1 class="flex gap-4 items-center text-5xl font-bold" style={{
-          filter: 'drop-shadow(0 0 30px var(--color-luminescent-400))',
-        }}>
-          <Box class="w-16 h-16" />
-          Mineplace
-        </h1>
-        <p>
-          Server IP:
+      <div class="animate-in fade-in slide-in-from-top-6 anim-duration-1000 lum-card p-16 shadow-xl">
+        <div class="flex gap-4 items-center mb-6">
+          <Box size={64} />
+          <h1 class="text-5xl font-bold">
+            Mineplace
+            <a class="text-lg flex items-center mt-2 hover:underline font-normal" href="https://birdflop.com">
+              <LogoBirdflop size={20} /> Powered by Birdflop Hosting
+            </a>
+          </h1>
+        </div>
+
+        <p class="text-4xl mb-6 font-bold">
+          Now imagine if r/place was 3D.
         </p>
-        <div class="lum-card p-2">
-          mineplace.me
+        <div class="flex gap-2">
+          <button class="lum-btn border-purple-500 border-2 hover:border-2 hover:lum-bg-purple-500 cursor-pointer">
+            <Network size={24} />
+            Server IP: mineplace.me
+          </button>
+          <div class="flex-1"></div>
+          <a class="lum-btn p-2 lum-bg-transparent" href="https://discord.gg/nmgtX5z">
+            <LogoDiscord size={24} />
+          </a>
+          <a class="lum-btn p-2 lum-bg-transparent" href="https://github.com/LuminescentDev">
+            <Github size={24} />
+          </a>
+          <a class="lum-btn p-2 lum-bg-transparent" href="https://luminescent.dev">
+            <LogoLuminescent size={24} />
+          </a>
+          <a class="lum-btn p-2 lum-bg-transparent" href="https://github.com/LuminescentDev">
+            <LogoBirdflop size={24} />
+          </a>
         </div>
       </div>
     </div>
